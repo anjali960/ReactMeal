11f3fe53b00713d2be90da0faa485e54edb4407c
anjali960 <75172674+anjali960@users.noreply.github.com>
2022-11-02 20:31:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </div>
+  );
+};
+export default App;
